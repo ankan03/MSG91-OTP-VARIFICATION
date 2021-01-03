fd1a76defadad475c36d84855352dcccac84e59c
ankan03 <1828049@kiit.ac.in>
2021-01-03 10:12:29 +0530

    MSG91 in JAVA official app

---
+++ app/src/main/java/com/example/msg91otpvarification/ApplicationClass.java
@@ -0,0 +1,13 @@
+package com.example.msg91otpvarification;
+
+import android.app.Application;
+
+import com.msg91.sendotpandroid.library.internal.SendOTP;
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SendOTP.initializeApp(this,"343141A0eUofjHNg5f73eff8P1");
+    }
+}
